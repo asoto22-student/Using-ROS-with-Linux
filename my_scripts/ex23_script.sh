c827d818155a69b14d18ea77ea173fd818ae1ebc
asoto22 <asoto22@masonlive.gmu.edu>
2020-09-04 03:14:49 +0000

    first commit

---
+++ my_scripts/ex23_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Changes directory to the my_scripts directory we made earlier
+cd ~/catkin_ws/src/linux_course_files/move_bb8_pkg/my_scripts 
+
+# Changes the permissions of the move_bb8_square.py file to give all groups full permissions
+chmod 777 move_bb8_square.py
+
+# Shows the files again
+ls -la
+
